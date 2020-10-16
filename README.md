ec99ee82c3a759d2bb1fe02a928274367766563b
Vincent Leblond <vincent.leblond@edgemind.net>
2020-10-16 09:23:38 +0200

    init repo

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # html2pdf
+
 Converting html ioslides files to pdf
+
+## Installation
+
+- Install nodejs and npm
+- Install puppeteer
+
+    ```bash
+    npm install --save puppeteer
+    ```
+
+## Usage
+
+```bash
+node hn.js inputdir outputdir
+```
+
+*inputdir* contains input html files.
---
+++ hn.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+const args = process.argv;
+
+if(args.length != 4) {
+  console.log("Path error")
+  console.log("Usage: node hn.js inputdir outputdir")
+} else {
+  const dirpath = path.resolve(args[2]);
+  const outpath = path.resolve(args[3]);
+  (async () => {
+    // Launch the browser
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+  
+    // Get all the .html filesystem items
+    let files = fs.readdirSync(dirpath).filter((f) => f.endsWith(".html"));
+    for (let f in files) {
+      console.log(files[f]);
+      let htmlFile = path.join(dirpath, files[f]);
+      // Skip anything that is not a file
+      if (!fs.statSync(htmlFile).isFile()) {
+        continue;
+      }
+      // Load the html
+      await page.goto("file://" + htmlFile);
+      // Export to PDF
+      await page.pdf({
+        path: path.join(outpath, files[f].replace(/\.html$/, ".pdf")),
+        format: "A4",
+        landscape: true,
+        printBackground: true,
+      });
+    }
+    // Close the browser
+    await browser.close();
+  })();
+}
+const arg = process.argv.pop();
+// Fallback to current dir if none is specified
+const dirpath = path.resolve(arg === __filename ? "." : arg);
+
